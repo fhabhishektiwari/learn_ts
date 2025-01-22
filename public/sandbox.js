dbe7364aa8de165858e777a924bc706d1c87b010
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-01-23 04:39:57 +0530

    #8 - Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("Hello");
+let greet;
+greet = () => {
+    console.log("Hello, world!");
+};
+// const add = (a: number, b: number, c?: number | string) => { // here c?: number | string is optional parameter
+//   console.log(a + b);
+//   console.log(c);
+// };
+const add = (a, b, c = 10) => {
+    //here c: number | string = 10 is default parameter and function is void type
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10);
+add(5, 10, "hello");
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7); //always specify the return type of the function is number
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,23 @@
-console.log("Hello");
+let greet: Function;
+
+greet = () => {
+  console.log("Hello, world!");
+};
+
+// const add = (a: number, b: number, c?: number | string) => { // here c?: number | string is optional parameter
+//   console.log(a + b);
+//   console.log(c);
+// };
+const add = (a: number, b: number, c: number | string = 10) => {
+  //here c: number | string = 10 is default parameter and function is void type
+  console.log(a + b);
+  console.log(c);
+};
+add(5, 10);
+add(5, 10, "hello");
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(10, 7); //always specify the return type of the function is number
