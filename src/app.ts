ad13b4c64d604c0e5c4c656cb7aa1a4b79b182f3
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-01-24 04:44:55 +0530

    #15 - Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
+// interface
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: "shaun",
+  age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log("I spent", amount);
+    return amount;
+  },
+};
+
+const greetPerson = (person: IsPerson) => {
+  console.log("hello", person.name);
+};
+
+greetPerson(me);
+
 import { Invoice } from "./classes/invoices.js";
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
