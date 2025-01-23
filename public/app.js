3af1fda63a189877563d751ea6ab299c36e09d06
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-01-23 06:10:26 +0530

    #12 - Classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,20 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+let invoices = [];
+invoices.push(invOne.format());
+invoices.push(invTwo.format());
+console.log(invoices);
 // console.log("Hello World");
 const anchor = document.querySelector("a");
 // if (anchor) {
