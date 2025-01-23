508a69b0caf48b10d247959d3462c1efb49f9a96
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-01-23 06:54:30 +0530

    #13 - Public, Private & Readonly

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
     return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -17,10 +17,13 @@ class Invoice {
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
-let invoices: string[] = [];
-invoices.push(invOne.format());
-invoices.push(invTwo.format());
-console.log(invoices);
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// console.log(invoices);
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 // console.log("Hello World");
 
