a794c79dce17cd37575b2b983553e5a15101eafa
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-01-24 06:19:53 +0530

    #18 - Generics

--- public/app.js
+++ public/app.js
@@ -23,3 +23,22 @@ form.addEventListener("submit", (e) => {
     }
     list.render(doc, type.value, "end");
 });
+// GENERICS
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: "yoshi", age: 40 });
+// let docTwo = addUID("hello");
+console.log(docOne.name);
+const docThree = {
+    uid: 1,
+    resourceName: "person",
+    data: { name: "shaun" },
+};
+const docFour = {
+    uid: 2,
+    resourceName: "shoppingList",
+    data: ["bread", "milk"],
+};
+console.log(docThree, docFour);
